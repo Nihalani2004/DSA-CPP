327179613d08edb2b40aa28e87830ba99e134432
Mayank Nihalani <162049530+Nihalani2004@users.noreply.github.com>
2026-08-22 03:16:21 +0530

    Time: 0 ms (100%), Space: 15 MB (25.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -18,28 +18,27 @@ public:
 
         //optimal
         int n=nums.size();
-        vector<pair<int, int>> arr;
-    for(int i = 0; i < n; i++) {
-    arr.push_back({nums[i], i});
-}
-sort(arr.begin(),arr.end());
+        vector<pair<int,int>> arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({nums[i],i});
+        }
+
+        sort(arr.begin(),arr.end());
 
         int left=0;
-        int right = n-1;
-        
-        
-        while(left<right){
-            int sum = arr[left].first + arr[right].first;
+        int right=n-1;
 
-            if(sum == target){
-                return {arr[left].second,arr[right].second};
-            }else if(sum < target){
+        while(left<right){
+            int sum= arr[left].first + arr[right].first;
+
+            if(sum==target){
+                return {arr[left].second, arr[right].second};
+            }else if(sum<target){
                 left++;
-            } else{
+            }else{
                 right--;
             }
         }
         return {-1,-1};
-        
     }
 };
