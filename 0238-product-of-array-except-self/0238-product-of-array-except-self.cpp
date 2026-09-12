aafbdfd3815fea0ee6791ae9f886c1fece2955ff
Mayank Nihalani <162049530+Nihalani2004@users.noreply.github.com>
2026-09-13 00:38:31 +0530

    Time: 0 ms (100%), Space: 40.3 MB (59.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -14,23 +14,27 @@ public:
 
         //optimal
         vector<int> ans(n,1);
-        vector<int> prefix(n,1);
-        vector<int> suffix(n,1);
+        // vector<int> prefix(n,1);
+        // vector<int> suffix(n,1);
 
-         //prefix
+         //prefix is directly stored in ans
          for(int i=1;i<n;i++){
-             prefix[i] = prefix[i-1] * nums[i-1];
+            //  prefix[i] = prefix[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
          }
 
          //suffix
+         int suffix=1;
          for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
+            // suffix[i] = suffix[i+1] * nums[i+1];
+            suffix = suffix*nums[i+1];
+            ans[i] = ans[i]*suffix;
          }
 
          //final ans
-         for(int i=0;i<n;i++){
-            ans[i] = prefix[i] * suffix[i];
-         }
+        //  for(int i=0;i<n;i++){
+        //     ans[i] = prefix[i] * suffix[i];
+        //  }
          return ans;
     }
 };
